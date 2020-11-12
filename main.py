99413e632eb8f75310bd1bab60bab3c0bfe03c0c
TheToxicPotato <50581889+kimm20@users.noreply.github.com>
2020-11-12 15:39:34 +0700

    It's not a virus... trust me

---
+++ main.py
@@ -0,0 +1,31 @@
+import random,time
+from playsound import playsound
+
+
+filename="sound.mp3"
+debugmode=False# if true will print current random number
+def rater():
+    rate=float(input("enter rate(1-100 higher=slower) : "))
+    rate=rate/10
+    return rate
+try:
+
+    rate=rater()
+    while rate>10.0:
+        print("Rlly?",rate,"s delay???")
+        rate=rater()
+    while rate < 0.1:
+        print("That's too fast!!",rate,"s delay is very fast!")
+        rate=rater()
+
+except:
+    print("e")
+    rate=1
+while True:
+    randomuwu=random.randrange(1,100)
+    if randomuwu % 5 == 0:
+        playsound(filename)
+    if debugmode:
+        print(randomuwu)
+    time.sleep(rate)
+#------------------------------------------------------------------------------#
